5487d063e8fba8afbc7f813c8a1ca914ba0db5fe
Jhonjs <jhon.sidney@gmail.com>
2023-03-17 11:40:34 -0300

    Add files via upload

--- aula 1/app.py
+++ aula 1/app.py
@@ -1,1 +1,14 @@
+from flask import Flask 
 
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return "index"
+
+@app.route("/teste")
+def teste():
+    return "teste"
+
+if __name__ == '__main__':
+    app.run()
--- aula 2/app.py
+++ aula 2/app.py
@@ -1,1 +1,19 @@
+from flask import Flask 
 
+app = Flask(__name__)
+
+@app.route('/hello/')
+@app.route('/hello/<name>')
+def hello(name=""):
+    return "<h1> Seja bem vindo{} </h1>".format(name)
+
+@app.route('/blog/')
+@app.route('/blog/<int:postID>')
+def blog(postID):
+    if postID >= 2:
+        return "blog info {}".format(postID)
+    else:
+        return " blog todo"
+
+if __name__ == '__main__':
+    app.run()
